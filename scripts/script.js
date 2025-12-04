b2196879d28e78fabe1e313a717c548c2df4e94e
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-12-04 21:13:20 +0800

    feat: Handle logic after move, printing and reset

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const GameBoard = (function () {
     const board = [];
 
     const initializeBoard = () =>  {
+        board.splice(0, board.length)
         for (let i = 0; i < 9; ++i) {
             board.push(createCell())
         }
@@ -15,10 +16,26 @@ const GameBoard = (function () {
         return board[index];
     }
 
+    const printBoard = () => {
+        console.log(`${getSymbol(0)} | ${getSymbol(1)} | ${getSymbol(2)}`)
+        console.log(`${getSymbol(3)} | ${getSymbol(4)} | ${getSymbol(5)}`)
+        console.log(`${getSymbol(6)} | ${getSymbol(7)} | ${getSymbol(8)}`)
+    }
+    
+    const getSymbol = (index) => {
+        const cell = getCell(index)
+        if (cell.isEmpty()) {
+            return "";
+        } else {
+            return GameLogic.getPlayerSymbol(cell.getPlayer());
+        }
+    }
+
     initializeBoard();
 
-    return { initializeBoard, getBoard, getCell };
+    return { initializeBoard, getBoard, getCell, printBoard };
 })();
+
 
 function createCell () {
     let player = null;
@@ -38,6 +55,7 @@ function createCell () {
     return { getPlayer, setPlayer, isEmpty };
 }
 
+
 function createPlayer (nameValue) {
     let name = nameValue;
 
@@ -46,24 +64,46 @@ function createPlayer (nameValue) {
     return { getName };
 }
 
+
 const GameLogic = (function () {
-    const players = [createPlayer("Player 1"), createPlayer("Player 2")];
-    let currentPlayer = players[0];
+    const players = {"x" : createPlayer("Player 1"), "o" : createPlayer("Player 2")};
+    let currentPlayer = players["x"];
     const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
                             [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    let movesTaken = 0
 
     const switchCurrentPlayer = () => {
-        currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+        currentPlayer = currentPlayer == players["x"] ? players["o"] : players["x"];
     }
 
-    const getCurrentPlayer = () => currentPlayer;
-
     const makeMove = (index) => {
-        cell = GameBoard.getCell(index);
+        const cell = GameBoard.getCell(index);
 
         if ( cell.isEmpty() ) {
             cell.setPlayer(currentPlayer);
+            gameLogicAfterMove();
         }
+    }
+
+    const gameLogicAfterMove = () => {
+        GameBoard.printBoard();
+        movesTaken++;
+
+        if (checkWin()) {
+            handleWin();
+        } else if (checkDraw()) {
+            handleDraw();
+        } else {
+            switchCurrentPlayer();
+        }
+    }
+
+    const handleWin = () => {
+        console.log(`${currentPlayer.getName()} has won the Game!!!`);
+    }
+
+    const handleDraw = () => {
+        console.log("You have reached a draw.");
     }
 
     const checkWin = () => {
@@ -76,6 +116,14 @@ const GameLogic = (function () {
         return false;
     }
 
+    const checkDraw = () => {
+        if (movesTaken == 9) {
+            return true;
+        }
+
+        return false;
+    }
+ 
     const allOccupiedByCurrentPlayer = (indicesArrray) => {
         for (let i = 0; i < indicesArrray.length; ++i) {
             if (notOccupiedByCurrentPlayer(indicesArrray[i])) {
@@ -90,5 +138,19 @@ const GameLogic = (function () {
         return GameBoard.getCell(index).getPlayer() !== currentPlayer;
     }
 
-    return { makeMove, checkWin }
+    const getPlayerSymbol = (player) => {
+        if (player === players["x"]) {
+            return "X";
+        } else {
+            return "O";
+        }
+    }
+
+    const resetGame = () => {
+        GameBoard.initializeBoard();
+        currentPlayer = players["x"];
+        movesTaken = 0;
+    }
+
+    return { makeMove, getPlayerSymbol, resetGame }
 })();
